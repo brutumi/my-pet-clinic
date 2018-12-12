1f5e58d6cbf998086c076ee63f234231b6907990
Jovan Jakimovski <jovanj9730@gmail.com>
2018-12-12 21:22:39 +0100

    Initial Project Add

---
+++ src/main/java/jovan/jakimovski/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,14 @@
+package jovan.jakimovski.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+
+}
+
