01c4130db5260ae5a0e85d39dc23d9212bc6d981
brutumi <jovanj9730@gmail.com>
2019-01-05 00:56:08 +0100

    Create Owner index page controller. Closes #14.

---
+++ pet-clinic-web/src/main/java/jovan/jakimovski/mypetclinic/controllers/OwnerController.java
@@ -0,0 +1,21 @@
+package jovan.jakimovski.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+/*
+    /owners
+    /owners/
+    /owners/index
+    /owners/index.html
+*/
+
+    @RequestMapping({"","/","/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
