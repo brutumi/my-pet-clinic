116d0de020740526b2b02b472650f5db577a43a4
brutumi <jovanj9730@gmail.com>
2019-01-09 22:41:59 +0100

    Added Speciality, updated vet with specialties. closes #29.

---
+++ pet-clinic-data/src/main/java/jovan/jakimovski/mypetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package jovan.jakimovski.mypetclinic.model;
+
+public class Speciality extends BaseEnitity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
