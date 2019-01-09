97e5f9365ff0854ca9aad72b2e0e93642293bcf7
brutumi <jovanj9730@gmail.com>
2019-01-09 22:22:02 +0100

    Added thymeleaf master layout to vet page. Closes #22.

--- pet-clinic-web/src/main/java/jovan/jakimovski/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/jovan/jakimovski/mypetclinic/controllers/VetController.java
@@ -1,25 +1,25 @@
-//package jovan.jakimovski.mypetclinic.controllers;
-//
-//import jovan.jakimovski.mypetclinic.services.VetService;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/vets")
-//public class VetController {
-//
-//    private final VetService vetService;
-//
-//    public VetController(VetService vetService) {
-//        this.vetService = vetService;
-//    }
-//
-//    @RequestMapping({"","/","/index", "/index.html"})
-//    public String listVets(Model model){
-//
-//        model.addAttribute("vets",vetService.findAll());
-//
-//        return "vets/index";
-//    }
-//}
+package jovan.jakimovski.mypetclinic.controllers;
+
+import jovan.jakimovski.mypetclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/","/index", "/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
+        return "vets/index";
+    }
+}
