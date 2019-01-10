c31e49cd8da479731c0aa39861f2dbb16f7bcb64
brutumi <jovanj9730@gmail.com>
2019-01-10 10:41:44 +0100

    Create Specialty Map Service Impl. closes #33.

---
+++ pet-clinic-data/src/main/java/jovan/jakimovski/mypetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package jovan.jakimovski.mypetclinic.services;
+
+import jovan.jakimovski.mypetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
