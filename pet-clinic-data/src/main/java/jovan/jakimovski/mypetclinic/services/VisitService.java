b9c157f83d5010d7e3d3f5dcb8847092f155436e
jovan <jovanj9730@gmail.com>
2019-04-09 12:09:34 +0200

    Added visit map service. closes #48

---
+++ pet-clinic-data/src/main/java/jovan/jakimovski/mypetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package jovan.jakimovski.mypetclinic.services;
+
+import jovan.jakimovski.mypetclinic.model.Vet;
+import jovan.jakimovski.mypetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
