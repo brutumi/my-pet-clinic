118135e24ae12a8f6991427579471afcff188db9
jovan <jovanj9730@gmail.com>
2019-04-09 14:52:49 +0200

    Added Project Lombok to project, closes #53

--- pet-clinic-web/src/main/java/jovan/jakimovski/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jovan/jakimovski/mypetclinic/bootstrap/DataLoader.java
@@ -35,6 +35,10 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadData() {
+
+        //BUILDER PATTERN
+        Owner.builder().firstName("adda").lastName("").build();
+
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
