3a61d989fa1103ae4c911104c94290cd28355184
jovan <jovanj9730@gmail.com>
2019-04-09 06:47:25 +0200

    updated visit entity. closes #40.

--- pet-clinic-data/src/main/java/jovan/jakimovski/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/jovan/jakimovski/mypetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package jovan.jakimovski.mypetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEnitity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>(); // inicijalizacija za da ne e null
 
     public String getName() {
         return name;
@@ -51,5 +56,13 @@ public class Pet extends BaseEnitity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
